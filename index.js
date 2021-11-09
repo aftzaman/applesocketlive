dd8ea164b4cb860aa91d205bdfd827a21f58575d
Aftaf <aftzaman@gmail.com>
2021-11-09 19:17:08 +0400

    all

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+'use strict';
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+const express = require('express');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-function onConnection(socket){
-  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
-}
-
-io.on('connection', onConnection);
-
-http.listen(port, () => console.log('listening on port ' + port));
-
-
-
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
