6f5ad080e7e45d921a80872ad176126c265cc55a
Aftaf <aftzaman@gmail.com>
2021-11-09 19:02:30 +0400

    temp

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const http = require('http');
-// const server = http.createServer(app);
-// const { Server } = require("socket.io");
-// const io = new Server(server);
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hello world</h1>');
-// });
-
-// io.on('connection', (socket) => {
-// 	console.log('a user connected');
-//   });
-
-// server.listen(3000, () => {
-//   console.log('listening on *:3000');
-// });
-
-
-// const express = require('express');
-// const app = express();
-// app.get('/', (req, res) => {
-//   res.send('<h1>Hello world</h1>');
-// });
-// const server = require('http').createServer();
-// const io = require('socket.io')(server);
-// io.on('connection', client => {
-// console.log('a user connected');
-//   client.on('event', data => { /* … */ });
-//   client.on('disconnect', () => { /* … */ });
-// });
-// server.listen(3000, () => {
-//   console.log('listening on *:3000');
-// });
-
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
@@ -50,3 +15,6 @@ function onConnection(socket){
 io.on('connection', onConnection);
 
 http.listen(port, () => console.log('listening on port ' + port));
+
+
+
