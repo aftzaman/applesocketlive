92bd15c57b2a820e27cacbbb4e2c0209ff719109
Habib <aftzaman@gmail.com>
2024-02-19 10:54:44 +0400

    Loading file modified

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-'use strict';
+// 'use strict';
 
-const express = require('express');
-const socketIO = require('socket.io');
+// const express = require('express');
+// const socketIO = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = 'views/home.html';
+// const PORT = process.env.PORT || 3000;
+// const INDEX = 'views/home.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+// const server = express()
+//   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = socketIO(server);
+// const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+// io.on('connection', (socket) => {
+//   console.log('Client connected');
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+// });
+
+// setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(express.static(__dirname + '/views'));
+app.use(bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true
+}));
+app.use(bodyParser.json({
+    limit: '50mb'
+}));
+
+var port = process.env.PORT || 3000;
+
+var router = express.Router();
+router.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+const server = app.listen(port, () => {
+    console.log('listening on *: ' + port);
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+console.log('Magic happens on port ' + port);
